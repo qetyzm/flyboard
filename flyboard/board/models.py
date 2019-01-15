b395250092a814980b12e2e5fb56a4a4d2a4fd01
qetyzm <qetyzm@gmail.com>
2019-01-15 13:57:42 +0100

    Extended board model

--- flyboard/board/models.py
+++ flyboard/board/models.py
@@ -19,3 +19,41 @@ class Board(Base):
     mute_not_original = db.Column(db.Boolean, nullable=False)
     extra_js = db.Column(db.String(10000))
     extra_css = db.Column(db.String(10000))
+    animated_thumbnails = db.Column(db.Boolean, nullable=False)
+
+    def __init__(
+        self, 
+        uri, 
+        title, 
+        min_files=1, 
+        max_files=1, 
+        file_required_for_post=False,
+        embeds_allowed=False,
+        ids_allowed=False,
+        sage_allowed=True,
+        sage_shown=True,
+        max_pages=10,
+        archive_pages=5,
+        max_file_size_in_mbs=10,
+        allowed_file_extensions="png,gif,jpg,jpeg,webm,mp4",
+        mute_not_original=False,
+        extra_js="",
+        extra_css="",
+        animated_thumbnails=False):
+        self.uri = uri
+        self.title = title
+        self.min_files = min_files
+        self.max_files = max_files
+        self.file_required_for_post = file_required_for_post
+        self.embeds_allowed = embeds_allowed
+        self.ids_allowed = ids_allowed
+        self.sage_allowed = sage_allowed
+        self.sage_shown = sage_shown
+        self.max_pages = max_pages
+        self.archive_pages = archive_pages
+        self.max_file_size_in_mbs = max_file_size_in_mbs
+        self.allowed_file_extensions = allowed_file_extensions
+        self.mute_not_original = mute_not_original
+        self.extra_js = extra_js
+        self.extra_css = extra_css
+        self.animated_thumbnails = animated_thumbnails
